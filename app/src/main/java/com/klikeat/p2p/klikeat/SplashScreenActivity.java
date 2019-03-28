4e73db7dc78d9410e427c5187b9a877cbb3437bd
Putra Utama <putrautama30@gmail.com>
2019-03-28 22:05:55 +0700

    addd login,register and splashscreen

--- app/src/main/java/com/klikeat/p2p/klikeat/SplashScreenActivity.java
+++ app/src/main/java/com/klikeat/p2p/klikeat/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.klikeat.p2p.klikeat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,32 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        startAnimation();
+    }
+
+    Thread splashThread;
+
+    private void startAnimation(){
+        splashThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    int waited =0;
+                    while (waited <3500){
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashScreenActivity.this.finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    SplashScreenActivity.this.finish();
+                }
+            }
+        };
+        splashThread.start();
     }
 }
