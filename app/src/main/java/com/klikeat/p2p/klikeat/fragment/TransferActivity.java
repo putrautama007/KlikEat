2e3950470057e1f8706466552962a682d7da0981
miftahfarhan13 <miftahfarhan20@gmail.com>
2019-04-05 16:48:20 +0700

    transfer activity, comment activity push commit

---
+++ app/src/main/java/com/klikeat/p2p/klikeat/fragment/TransferActivity.java
@@ -0,0 +1,15 @@
+package com.klikeat.p2p.klikeat.fragment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.klikeat.p2p.klikeat.R;
+
+public class TransferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+    }
+}
