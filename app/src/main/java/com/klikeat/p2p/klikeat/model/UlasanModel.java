77782c157b37f71b81efa5aaac414bea1f2b480d
Putra Utama <putrautama30@gmail.com>
2019-04-15 08:09:30 +0700

    fix layouting

--- app/src/main/java/com/klikeat/p2p/klikeat/model/UlasanModel.java
+++ app/src/main/java/com/klikeat/p2p/klikeat/model/UlasanModel.java
@@ -1,15 +1,17 @@
 package com.klikeat.p2p.klikeat.model;
 
 public class UlasanModel {
-    public String namaProfile,isiUlasan,ratingProduk,idUlasan,tglUlasan;
+    public String namaProfile,isiUlasan,ratingProduk,idUlasan,tglUlasan,produk_id,userId;
 
-    public UlasanModel(String namaProfile, String isiUlasan,
-                       String ratingProduk, String idUlasan, String tglUlasan) {
+    public UlasanModel(String namaProfile, String isiUlasan, String ratingProduk, String idUlasan,
+                       String tglUlasan, String produk_id, String userId) {
         this.namaProfile = namaProfile;
         this.isiUlasan = isiUlasan;
         this.ratingProduk = ratingProduk;
         this.idUlasan = idUlasan;
         this.tglUlasan = tglUlasan;
+        this.produk_id = produk_id;
+        this.userId = userId;
     }
 
     public UlasanModel() {
